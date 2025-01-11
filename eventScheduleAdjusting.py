3bc7ba51a5d61842bf57d4e59c9705c6d666bc2c
dawnfire05 <dawnfire05@naver.com>
2025-01-12 01:43:00 +0900

    feat: retrieve data from db

--- eventScheduleAdjusting.py
+++ eventScheduleAdjusting.py
@@ -3,9 +3,16 @@ from slack_sdk import WebClient
 from slack_sdk.errors import SlackApiError
 import os
 from datetime import datetime, timedelta
+import boto3
+import traceback
+from boto3.dynamodb.conditions import Key
+
 
 SLACK_BOT_TOKEN = os.environ['SLACK_BOT_TOKEN']
 slack_client = WebClient(token=SLACK_BOT_TOKEN)
+dynamodb = boto3.resource("dynamodb")
+table = dynamodb.Table('testDB')
+
 
 def date_to_weekdays(start_date, end_date):
     start = datetime.strptime(start_date, "%Y-%m-%d")
@@ -30,7 +37,7 @@ def is_time_overlapping(time_slot, duration, start_time, end_time):
     end = time_to_minutes(end_time)
     return not (slot_end <= start or slot_start >= end)
 
-def find_best_time_slot(users_schedule, mandatory_person, duration, weekdays):
+def find_best_time_slot(users_schedule, user_id, duration, weekdays):
     time_slots = [f"{hour:02d}:{minute:02d}" for hour in range(8, 24) for minute in range(0, 60, 30)]
     best_time_slot = None
     max_participants = 0
@@ -40,7 +47,7 @@ def find_best_time_slot(users_schedule, mandatory_person, duration, weekdays):
             participants = 0
             unavailable = []
             for person, schedule in users_schedule.items():
-                if person in mandatory_person:
+                if person in user_id:
                     available = True
                     for wday, start_time, end_time in schedule:
                         if wday == day and is_time_overlapping(time_slot, duration, start_time, end_time):
@@ -67,34 +74,34 @@ def find_best_time_slot(users_schedule, mandatory_person, duration, weekdays):
 def lambda_handler(event, context):
     # 불가능한 시간 조정
 
+    participants_id = ['U0792169H4Y', 'U0430P2DSVA', 'UH73HNUFR']
+
     ## 알고리즘으로 시간표 다시 계산
+    data = []
 
-    data = '''
-    [
-        {
-            "name": "서지은",
-            "createdAt": "2025-01-11",
-            "schedule": "{ \\"Monday\\": [ { \\"start_time\\": \\"08:00\\", \\"end_time\\": \\"12:30\\", \\"name\\": \\"사회물리학: 네트워크적 접근\\", \\"index\\": 1 }, { \\"start_time\\": \\"14:30\\", \\"end_time\\": \\"15:30\\", \\"name\\": \\"AI 원리 및 최신기술\\", \\"index\\": 2 } ], \\"Tuesday\\": [ { \\"start_time\\": \\"09:00\\", \\"end_time\\": \\"10:00\\", \\"name\\": \\"기계학습\\", \\"index\\": 3 } ], \\"Wednesday\\": [ { \\"start_time\\": \\"13:00\\", \\"end_time\\": \\"15:00\\", \\"name\\": \\"심리학\\", \\"index\\": 4 } ] }"
-        },
-        {
-            "name": "서은원",
-            "createdAt": "2025-01-11",
-            "schedule": "{ \\"Monday\\": [ { \\"start_time\\": \\"11:00\\", \\"end_time\\": \\"12:30\\", \\"name\\": \\"사회물리학: 네트워크적 접근\\", \\"index\\": 1 }, { \\"start_time\\": \\"14:30\\", \\"end_time\\": \\"15:30\\", \\"name\\": \\"AI 원리 및 최신기술\\", \\"index\\": 2 } ], \\"Tuesday\\": [ { \\"start_time\\": \\"09:00\\", \\"end_time\\": \\"10:00\\", \\"name\\": \\"기계학습\\", \\"index\\": 3 } ], \\"Wednesday\\": [ { \\"start_time\\": \\"13:00\\", \\"end_time\\": \\"15:00\\", \\"name\\": \\"심리학\\", \\"index\\": 4 } ] }"
-        }
-    ]
-    '''
+    try:
+        for participant_id in participants_id:
+            response = table.query(
+                KeyConditionExpression = Key('name').eq(participant_id)
+            )
+            data.extend(response.get('Items', []))
+        print(f"DB 불러오기 성공 : {data}")
+    except Exception as e:
+        print(f"DB 불러오기 실패 : {e}")
+        print(traceback.format_exc())
 
-    parsed_data = json.loads(data)
+
+    # parsed_data = json.loads(data)
     users_schedule = {}
 
     start_date = "2025-01-11"
     end_date = "2025-01-13"
-    mandatory_person = ["서은원"]
+    # user_id = ["서은원"]
     duration = 0.5  # 30분
 
 
-    for user in parsed_data:
-        name = user["name"]
+    for user in data:
+        participant_id = user["name"]
         schedule_data = json.loads(user["schedule"])
         times = []
         for day, day_schedule in schedule_data.items():
@@ -102,11 +109,11 @@ def lambda_handler(event, context):
                 start_time = item["start_time"]
                 end_time = item["end_time"]
                 times.append((day, start_time, end_time))
-        users_schedule[name] = times
+        users_schedule[participant_id] = times
 
     weekdays = date_to_weekdays(start_date, end_date)
 
-    print(find_best_time_slot(users_schedule, mandatory_person, duration, weekdays))
+    print(find_best_time_slot(users_schedule, participants_id, duration, weekdays))
 
     """
     특정 슬랙 스레드의 메시지를 가져오는 함수
